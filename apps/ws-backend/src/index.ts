d7cc35dc6726af76b872f527a9725f88022ed0cc
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-07-20 14:14:07 +0200

    Implements authentication check at websocket-backend
    Check jwt sent as url param at websocket backend.
    Uses local .env file to read JWT_SECRET as defined in common-backend package

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,17 +1,46 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from "jsonwebtoken"
+import {getJwtSecret} from "@repo/backend-common/jwt_secret"
+import "dotenv/config"
 
 const wss = new WebSocketServer({ port: 8080 });
 
-
 // need to write JWT logic on the ws-backend as well
 
-
+// something like *upgrade* request
 wss.on('connection', function connection(ws, req) {
-  ws.on('error', console.error);
+    // how will our request look like:
+    // ws://localhost:8080?token=123123
+    // we need to extract token value from url params
+    const url = req.url;
+    if(!url) {
+        // here we can do something even better
+        // initial req can be intercepted before a websocket connection is established
+        ws.close();
+        return;
+    }
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+    //extract queryParams from URL
+    const urlParams = new URLSearchParams(url.split("?")[1])
+    const token = urlParams.get("token") || "";
 
-  ws.send('something');
+    const decoded = jwt.verify(token, getJwtSecret())
+    if (typeof decoded === "string") {
+            ws.close()
+            return;    
+        }
+    if(!decoded || !(decoded as JwtPayload).userID) {
+        ws.close();
+        return;
+    }
+    // console.log('Received WebSocket connection with param:', paramValue);
+    const userID = decoded.userID;
+
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
 });
