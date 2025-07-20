b2a4307aaf772261542ea5b317ecebc3038d939b
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-07-20 12:07:32 +0200

    Setup boilerplate endpoints signup,signin,create-room for HTTP backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,10 +2,30 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.post("/signup", (req, res) => {
+    // do zod validation here
     res.json({
-        message: "Hello from HTTP backend"
+        message: "Sign up sucessfull",
+        userID: "123123"
     })
 })
 
-app.listen(3000)
+app.post("/signin", (req, res) => {
+    // do zod validation here
+    res.json({
+        message: "Sign in sucessfull",
+        token: "123123"
+    })
+})
+
+app.post("/create-room", (req, res) => {
+    // do zod validation here
+    res.json({
+        message: "Room created",
+        roomID: "123123"
+    })
+})
+
+app.listen(3000, () => {
+    console.log("HTTP server running on: http://localhost:3000")
+})
